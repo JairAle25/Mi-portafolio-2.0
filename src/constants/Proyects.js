a7c7a0af0df64cf1b6710d3973b69eddbfa5f1f5
Jair Cornejo <jaircornejo61@gmail.com>
2024-02-01 15:08:10 -0300

    poniendo links a los proyectos

--- src/constants/Proyects.js
+++ src/constants/Proyects.js
@@ -5,43 +5,57 @@ const proyects = [
         title:"Mi Portafolio",
         description:"Explora mi mundo a través de mi portafolio, donde convergen mi pasión, habilidades y proyectos destacados. Descubre quién soy, lo que sé hacer y sumérgete en los proyectos que han dado forma a mi trayectoria. Desde desarrollo web hasta diseño creativo, cada proyecto cuenta una historia única de dedicación y excelencia.",
         urlImage:"/ProyectsImages/portafolio.webp",
-        technologies:[TagsTech.ASTRO,TagsTech.TAILWIND]
+        technologies:[TagsTech.ASTRO,TagsTech.TAILWIND],
+        linkPagina:"https://portafolio-jair.netlify.app/",
+        linkRepositorio:"https://github.com/JairAle25/Mi-portafolio-2.0"
     },
     {
         title:"Lista de Tareas",
         description:"Explora la eficiencia y la organización con mi proyecto de lista de tareas. Esta aplicación te permite gestionar tus responsabilidades diarias de manera simple y efectiva. Escribe tus tareas, marca las completadas y elimina las que ya no necesitas. Experimenta la comodidad de un sistema intuitivo que te ayuda a mantener el control y a lograr tus metas de manera ordenada.  ",
         urlImage:"/ProyectsImages/listaDeTareas.webp",
-        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT,TagsTech.REACT]
+        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT,TagsTech.REACT],
+        linkPagina:"https://task-list-proyect.netlify.app/",
+        linkRepositorio:"https://github.com/JairAle25/TaskList-Proyect"
     },
     {
         title:"Clases Particulares",
         description:"En mi proyecto, he desarrollado una página dedicada a simplificar la gestión de clases particulares de programación para mis alumnos. En ella, encontrarás información detallada sobre las clases ofrecidas, horarios disponibles y un sistema eficiente de reserva. La página está diseñada pensando en la comodidad y eficacia, permitiendo a mis alumnos enfocarse en su aprendizaje.",
         urlImage:"/ProyectsImages/clasesParticulares.webp",
-        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT]
+        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT],
+        linkPagina:"https://jairale25.github.io/ClasesParticulares/",
+        linkRepositorio:"https://github.com/JairAle25/ClasesParticulares"
     },
     {
         title:"JcCodeWeb - Cursos Online",
         description:"Mi proyecto se centra en la promoción y oferta de cursos de programación. Aquí, los estudiantes pueden acceder a una variedad de cursos diseñados para todos los niveles de habilidad, desde principiantes hasta avanzados. Ofrezco lecciones interactivas, proyectos prácticos y un espacio para que los entusiastas de la programación amplíen sus habilidades.",
         urlImage:"/ProyectsImages/JcCodeWeb.webp",
-        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT,TagsTech.REACT]
+        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT,TagsTech.REACT],
+        linkPagina:"https://jccodeweb.netlify.app/",
+        linkRepositorio:"https://github.com/JairAle25/JcCodeWeb"
     },
     {
         title:"Conecta 4",
         description:"Descubre la emoción del clásico juego de estrategia con mi creación: el Conecta 4. Sumérgete en la diversión competitiva mientras planificas tus movimientos para alinear cuatro fichas antes que tu oponente. Disfruta de la simplicidad del juego, pero con la estrategia desafiante que lo convierte en un verdadero clásico.",
         urlImage:"/ProyectsImages/conecta4.webp",
-        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT,TagsTech.REACT]
+        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT,TagsTech.REACT],
+        linkPagina:"conecta-4-jair.netlify.app",
+        linkRepositorio:"https://github.com/JairAle25/Conecta-4"
     },
     {
         title:"Ta Te Ti",
         description:"Te presento mi proyecto de juego del Ta-Te-Ti, una experiencia clásica de entretenimiento. Diseñado para ser jugado localmente, este juego ofrece la oportunidad de disfrutar partidas rápidas y divertidas con amigos en la misma pantalla. La simplicidad del juego se combina con la emoción de la competencia cara a cara, proporcionando una experiencia nostálgica y agradable para todos.",
         urlImage:"/ProyectsImages/tateti.webp",
-        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT,TagsTech.REACT]
+        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT,TagsTech.REACT],
+        linkPagina:"https://ta-te-ti-jair.netlify.app/",
+        linkRepositorio:"https://github.com/JairAle25/Ta-Te-Ti"
     },
     {
         title:"Pokedex",
         description:"Presento mi proyecto de Pokedex, una herramienta interactiva que te permite explorar el fascinante mundo de los Pokémon. Simplemente elige un tipo, como fuego, y descubre una lista completa de Pokémon de ese tipo, junto con sus estadísticas. Esta Pokedex proporciona una visión completa y estructurada para los amantes de Pokémon.",
         urlImage:"/ProyectsImages/pokedex.webp",
-        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT]
+        technologies:[TagsTech.HTML,TagsTech.CSS,TagsTech.JAVASCRIPT],
+        linkPagina:"https://jairale25.github.io/Proyecto-Pokemon",
+        linkRepositorio:"https://github.com/JairAle25/Proyecto-Pokemon"
     }
 ]
 
