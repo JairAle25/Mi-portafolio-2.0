8e117eeb129e9d6c28051186c425afe8abd8119e
Jair Cornejo <jaircornejo61@gmail.com>
2024-01-31 22:10:17 -0300

    siguiendo con los proyectos

---
+++ src/constants/Proyects.js
@@ -0,0 +1,12 @@
+import TagsTech from "./tagsTech";
+
+const proyects = [
+    {
+        title:"Mi Portafolio",
+        description:"lorem asdasjdkahsdkhasjdh sdhaksdhajksh dajshdajshdjah kshdajkshdjahs jdhadhajkdhajkshdadhajksdhajs sjdhajsdhajkshdkajsh sdhak",
+        urlImage:"/ProyectsImages/portafolio.webp",
+        technologies:[TagsTech.ASTRO,TagsTech.TAILWIND]
+    }
+]
+
+export default proyects;
