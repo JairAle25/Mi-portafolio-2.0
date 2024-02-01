293a2f2db700614623b3db5fba40c23023f5d251
Jair Cornejo <jaircornejo61@gmail.com>
2024-01-31 21:45:19 -0300

    colocando constantes

---
+++ src/constants/Experience.js
@@ -0,0 +1,22 @@
+const experience = [
+    {
+        title : "Profesor de Programacion",
+        date:"Diciembre 2022 - Actualmente",
+        description:"Como instructor particular especializado, he impartido clases de programación en el ámbito de la Tecnicatura Universitaria en Programación. Mi enfoque se centra en las materias de C++, C#, .NET y SQL Server, brindando a los estudiantes una sólida base en estas tecnologías fundamentales. Además, he diseñado y conducido cursos de desarrollo web, proporcionando a los participantes las habilidades necesarias para destacar en el campo. Mi compromiso con la enseñanza y mi profundo conocimiento técnico han contribuido al éxito académico y profesional de mis alumnos.",
+        current:true
+    },
+    {
+        title : "Ayudante de catedra - Universidad Tecnologia Nacional",
+        date:"Junio 2023 - Diciembre 2023",
+        description:"Desempeñé el rol de ayudante de cátedra en dos asignaturas fundamentales: 'Programación 1' y 'Laboratorio de Computación 1' en la Universidad Tecnológica Nacional. Mi labor abarcó desde brindar apoyo integral a los estudiantes en sus proyectos y tareas, hasta la corrección de exámenes. Asimismo, me encargué de transmitir conceptos clave, contribuyendo al entendimiento profundo de los fundamentos de la programación. Esta experiencia fortaleció mi capacidad para facilitar el aprendizaje y apoyar el desarrollo académico de los estudiantes.",
+        current:null
+    },
+    {
+        title : "Front End Developer Freelance",
+        date:"Junio 2022 - Julio 2022",
+        description:"Como freelance, diseñé y construí desde cero la página web para una heladería en España 'Holy Madre'. Utilicé HTML, CSS, JavaScript y Bootstrap para crear páginas de contacto, mostrar variedades de helado, y más. Subí el sitio a la nube, asegurando una presencia online eficiente y atractiva. Este proyecto destaca mi habilidad para ofrecer soluciones web completas y personalizadas.",
+        current:null
+    }
+]
+
+export default experience;
---
+++ src/constants/LinksHeader.js
@@ -0,0 +1,28 @@
+const Links = [
+    {
+        name:"Inicio",
+        link:"#hero",
+
+    },
+    {
+        name:"Acerca de mi",
+        link:"#about",
+    },
+    {
+        name:"Experiencia",
+        link:"#experiencia",
+    },
+    {
+        name:"Tecnologias",
+        link:"#technologies",
+    },
+    {
+        name:"Proyectos",
+        link:"#proyectos",
+    },
+    {
+        name:"Contáctame",
+        link:""
+    }
+]
+export default Links;
